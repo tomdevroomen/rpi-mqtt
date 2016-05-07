15819173301245656ba75ea58e1bdb7e0e5e4ab1
Tom de Vroomen <social@tomdevroomen.nl>
2016-05-07 13:18:25 +0200

    Create mqttBroker, non-persistant

--- server/server.js
+++ server/server.js
@@ -1,46 +1,51 @@
 var mosca = require('mosca');
-
-var pubsubsettings = {
-    type: 'mongo',
-    url: 'mongodb://localhost:27017/mqtt',
-    pubsubCollection: 'myCollections',
-    mongo: {}
-};
-
 var settings = {
     port: 1883,
-    backend: pubsubsettings
+    persistence: mosca.persistence.Memory
+};
+//here we start mosca
+var server = new mosca.Server(settings, function () {
+    console.log('Mosca server is up and running')
+});
+
+server.published = function(packet, client, cb) {
+    if (packet.topic.indexOf('echo') === 0) {
+        return cb();
+    }
+
+    var newPacket = {
+        topic: 'echo/' + packet.topic,
+        payload: packet.payload.toString(),
+        retain: packet.retain,
+        qos: packet.qos
+    };
+
+    console.log('['+Date() +'] newPacket', newPacket);
+
+    server.publish(newPacket, cb);
 };
 
-//here we start mosca
-var server = new mosca.Server(settings);
-
-server.on('ready', setup);
-// fired when the mqtt server is ready
-function setup() {
-    console.log('Mosca server is up and running')
-}
 // fired when a client is connected
-server.on('clientConnected', function (client) {
+server.on('clientConnected', function(client) {
     console.log('client connected', client.id);
 });
 // fired when a message is received
-server.on('published', function (packet, client) {
+server.on('published', function(packet, client) {
     console.log('Published : ', packet.payload);
 });
 // fired when a client subscribes to a topic
-server.on('subscribed', function (topic, client) {
+server.on('subscribed', function(topic, client) {
     console.log('subscribed : ', topic);
 });
 // fired when a client subscribes to a topic
-server.on('unsubscribed', function (topic, client) {
+server.on('unsubscribed', function(topic, client) {
     console.log('unsubscribed : ', topic);
 });
 // fired when a client is disconnecting
-server.on('clientDisconnecting', function (client) {
+server.on('clientDisconnecting', function(client) {
     console.log('clientDisconnecting : ', client.id);
 });
 // fired when a client is disconnected
-server.on('clientDisconnected', function (client) {
+server.on('clientDisconnected', function(client) {
     console.log('clientDisconnected : ', client.id);
 });
